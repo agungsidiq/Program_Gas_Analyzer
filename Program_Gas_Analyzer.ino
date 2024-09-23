c215ae07e5546430aa2d3c5250f20d979c3ccf96
agungsidiq <90184607+agungsidiq@users.noreply.github.com>
2024-09-23 21:38:59 +0700

    update suhu

--- Program_Gas_Analyzer.ino
+++ Program_Gas_Analyzer.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature suhu(&oneWire);
+
+void setup() {
+  Serial.begin(9600);
+
+  suhu.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  suhu.requestTemperatures();
+  float suhuC = suhu.getTempCByIndex(0);
 
+  if(suhuC == DEVICE_DISCONNECTED_C){
+    suhuC = 0;
+  }
+
+  float pembanding_suhu = 5;
+  suhuC = suhuC + pembanding_suhu; // Bisa ditambah atau dikurangi
+
+  Serial.print("Suhu Oli : ");
+  Serial.println(suhuC);
 }
